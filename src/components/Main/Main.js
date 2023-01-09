21e7fce2d708355ec18f8712ce7c676ed0509be3
Michelle Ross <miross629@gmail.com>
2023-01-09 14:48:23 -0800

    Loop through and render Animal component. TODO: display image and sound

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main className="main" style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main className="main" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.type} image={animal.image} name={animal.name} sound={animal.says} />
+      ))}
+    </main>
+  );
 }
