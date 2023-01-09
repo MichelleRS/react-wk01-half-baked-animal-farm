96996a021b67f8e25f8acf6a46ea518d2686fbb3
Michelle Ross <miross629@gmail.com>
2023-01-09 15:17:02 -0800

    Display animal containers with top and left

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,14 @@ export default function Main() {
   return (
     <main className="main" style={{ backgroundImage: `url(${background})` }}>
       {animals.map((animal) => (
-        <Animal key={animal.type} image={animal.image} name={animal.name} sound={animal.says} />
+        <Animal
+          key={animal.type}
+          image={animal.image}
+          name={animal.name}
+          sound={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
       ))}
     </main>
   );
